0cb0e4aae341a13fd47b19e2d56812b550792773
Jor_Poon <jormpoon@gmail.com>
2019-03-21 13:18:21 -0400

    changes

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+require('dotenv').config();
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+//make port dynamic
+// yarn add dotenv
+//require dotenv config
+//create .env file
+const port = process.env.PORT || 4000;
+const greeting = process.env.GREETING;
+
+server.listen(port, () => {
+  console.log(`\n*** ${greeting} on http://localhost:${port} ***\n`);
 });
